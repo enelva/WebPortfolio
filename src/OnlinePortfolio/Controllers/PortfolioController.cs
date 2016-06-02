f6e34b1f5a99a0b1519ee94c9141e2a9fb4f1a9d
Mikael Brunnberg <mikael.brunnberg@gmail.com>
2016-06-02 10:47:17 +0200

    My first commit

---
+++ src/OnlinePortfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Http;
+using OnlinePortfolio.Models;
+using OnlinePortfolio.ViewModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OnlinePortfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendAnEmail(MailSenderVM mailSenderVMThing) // Lägg in en VM för det!
+        {
+            MailSender mail = new MailSender();
+            mail.SendMail(mailSenderVMThing.Name, mailSenderVMThing.Subject, mailSenderVMThing.Email, mailSenderVMThing.Message); //From, Subject, Email, Text
+            ViewData["mail_succes"] = "1";
+            return RedirectToAction(nameof(PortfolioController.Home));
+        }
+
+    }
+}
---
+++ src/OnlinePortfolio/Models/MailSender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace OnlinePortfolio.Models
+{
+    public class MailSender
+    {
+        public void SendMail(string fromName, string theSubject, string fromEmail, string textMessage)
+        {
+            // Sending from and to... Maybee...
+            MailMessage mail = new MailMessage("inbetween.musicgroup@gmail.com", "Mikael.Brunnberg@gmail.com");
+
+            mail.Subject = theSubject;
+
+            mail.Body = textMessage + "\n" + "\n" + "From: " + fromName + "\n" + "Email: " + fromEmail + "\n\n" + "(This message was sent to you from your web portfolio.)";
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587; // Koden är specifik för gmail
+
+            // Nedan är mailadressen samt lösenordet som epostmeddelandet skickas FRÅN
+            smtp.Credentials = new NetworkCredential("inbetween.musicgroup@gmail.com", "MikaelBrunnberg"); // (Skickas från epost, lösenordet till den eposten)
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+    }
+}
+
